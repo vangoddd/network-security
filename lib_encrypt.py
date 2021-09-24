d4bc512eb9474630b4266146c5c37518a6fea9c9
Vangod <vachri.attala@gmail.com>
2021-09-25 00:38:45 +0700

    working

--- lib_encrypt.py
+++ lib_encrypt.py
@@ -1,21 +1,39 @@
+from os import read
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-
-key = get_random_bytes(16)
+from base64 import b64encode
 
 def decrypt(encrypted, nonce, key):
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
     plaintext = cipher.decrypt(encrypted)
-    print(plaintext)
+    print("decrypted msg : ", plaintext.decode())
 
-def encrypt(raw, key):
+def encrypt(raw):
+    key = get_random_bytes(16)
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
     ciphertext = cipher.encrypt(raw)
-    print(ciphertext, nonce)
 
-    decrypt(ciphertext, nonce, key)
+    file_out = open("encrypted.bin", "wb")
+    file_out.write(nonce)
+    file_out.write(ciphertext)
+    file_out.close()
 
+    file_key = open("key.bin", "wb")
+    file_key.write(key)
+    file_key.close()
 
+#msg = input("your msg: ")
+msg = "test message"
+encrypt(msg.encode())
 
-encrypt(b'secret message', key)
+file_in = open("encrypted.bin", "rb")
+nonce = file_in.read(16)
+ciphertext = file_in.read()
+file_key = open("key.bin", "rb")
+newkey = file_key.read()
+
+file_in.close()
+file_key.close()
+
+decrypt(ciphertext, nonce, newkey)
